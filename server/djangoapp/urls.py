05a7c3e4c7906492a397cdf96e747275bbb17be3
dev-cynic <htetaunglynn@my.uopeople.edu>
2025-11-19 10:44:55 -0500

    Fixed indentation

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -16,17 +16,17 @@ urlpatterns = [
     # path for get dealers
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(
-        route='get_dealers/<str:state>', 
+        route='get_dealers/<str:state>',
         view=views.get_dealerships,
         name='get_dealers_by_state'),
     # path for get dealer details
     path(
-        route='dealer/<int:dealer_id>', 
+        route='dealer/<int:dealer_id>',
         view=views.get_dealer_details,
         name='dealer_details'),
     # path for get dealer reviews
     path(
-        route='reviews/dealer/<int:dealer_id>', 
+        route='reviews/dealer/<int:dealer_id>',
         view=views.get_dealer_reviews,
         name='dealer_reviews'),
     # path for add review
